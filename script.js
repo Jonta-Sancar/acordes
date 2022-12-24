9bf215457739426926790be21d375c334b4c76e3
Jonta Sancar <109826535+Jonta-Sancar@users.noreply.github.com>
2022-12-23 23:33:31 -0300

    optimize the features

--- script.js
+++ script.js
@@ -2,94 +2,25 @@ const span_tone = document.querySelector('#tone');
 const span_chord = document.querySelector('#chord');
 const span_grau = document.querySelector('#grau');
 const notes = [ 'c', 'c-sus', 'd', 'd-sus', 'e', 'f', 'f-sus', 'g', 'g-sus', 'a', 'a-sus', 'b'];
+const tone_major = [0, 2, 4, 5, 7, 9, 11];
+const tone_minor = [0, 2, 3, 5, 7, 8, 10];
 
-const tones = {
-  'c': [ 'c', 'dm', 'em', 'f', 'g', 'am', 'bd' ],
-  'cm': [ 'cm', 'd', 'd-sus', 'f', 'g', 'g-sus', 'a-sus' ],
-  'c-sus': [ 'c-sus', 'd-sus', 'f', 'f-sus', 'g-sus', 'a-sus', 'c' ],
-  'c-susm': [ 'c-susm', 'd-sus', 'e', 'f-sus', 'g-sus', 'a', 'b' ],
+const chord_major = [0, 4, 7];
+const chord_minor = [0, 3, 7];
+const chord_diminished = [0, 3, 6];
 
-  'd': [ 'd', 'e', 'f-sus', 'g', 'a', 'b', 'c-sus' ],
-  'dm': [ 'dm', 'e', 'f', 'g', 'a', 'a-sus', 'c' ],
-  'd-sus': [ 'd-sus', 'f', 'g', 'g-sus', 'a-sus', 'c', 'd' ],
-  'd-susm': [ 'd-susm', 'f', 'f-sus', 'g-sus', 'a-sus', 'b', 'c-sus' ],
+let chords = {}
 
-  'e': [ 'e', 'f-sus', 'g-sus', 'a', 'b', 'c-sus', 'd-sus' ],
-  'em': [ 'em', 'f-sus', 'g', 'a', 'b', 'c', 'd' ],
-
-  'f': [ 'f', 'g', 'a', 'a-sus', 'c', 'd', 'e' ],
-  'fm': [ 'fm', 'g', 'g-sus', 'a-sus', 'c', 'c-sus', 'd-sus' ],
-  'f-sus': [ 'f-sus', 'g-sus', 'a-sus', 'b', 'c-sus', 'd-sus', 'f' ],
-  'f-susm': [ 'f-susm', 'g-sus', 'a-sus', 'b', 'c-sus', 'd', 'e' ],
-
-  'g': [ 'g', 'a', 'b', 'c', 'd', 'e', 'f-sus' ],
-  'gm': [ 'gm', 'a', 'a-sus', 'c', 'd', 'd-sus', 'f' ],
-  'g-sus': [ 'g-sus', 'a-sus', 'c', 'c-sus', 'd-sus', 'f', 'g' ],
-  'g-susm': [ 'g-susm', 'a-sus', 'b', 'c-sus', 'd-sus', 'e', 'f-sus' ],
-
-  'a': [ 'a', 'b', 'c-sus', 'd', 'e', 'f-sus', 'g-sus' ],
-  'am': [ 'am', 'b', 'c', 'd', 'e', 'f', 'g' ],
-  'a-sus': [ 'a-sus', 'c', 'd', 'd-sus', 'f', 'g', 'a' ],
-  'a-susm': [ 'a-susm', 'c', 'c-sus', 'd-sus', 'f', 'f-sus', 'g-sus' ],
-
-  'b': [ 'b', 'c-sus', 'd-sus', 'e', 'f-sus', 'g-sus','a-sus' ],
-  'bm': [ 'bm', 'c-sus', 'd', 'e', 'f-sus', 'g', 'a' ],
-}
-
-const chords = {
-  'c':[ 0, 4, 7 ],
-  'cm':[ 0,3, 7 ],
-  'cd':[ 0, 3, 6, ],
-  'c-sus':[1, 5, 8 ],
-  'c-susm':[ 1, 4, 8 ],
-  'c-susd':[ 1, 4, 7 ],
-  
-  'd':[ 2, 6, 9 ],
-  'dm':[ 2, 5, 9 ],
-  'dd':[ 2, 5, 8 ],
-  'd-sus':[ 3, 7, 10],
-  'd-susm':[ 3, 6, 10 ],
-  'd-susd':[ 3, 6, 9 ],
-
-  'e':[ 4, 8, 11],
-  'em':[ 4, 7, 11 ],
-  'ed':[ 4, 7, 10 ],
-
-  'f':[ 5, 9, 0],
-  'fm':[ 5, 8, 0 ],
-  'fd':[ 5, 8, 11 ],
-  'f-sus':[ 6, 10, 1 ],
-  'f-susm':[ 6, 9, 1 ],
-  'f-susd':[ 6, 9, 0 ],
-
-  'g':[ 7, 11, 2],
-  'gm':[ 7, 10, 2 ],
-  'gd':[ 7, 10, 1 ],
-  'g-sus':[ 8, 0, 3 ],
-  'g-susm':[ 8, 11, 3 ],
-  'g-susd':[ 8, 11, 2 ],
-
-  'a':[ 9, 1, 4 ],
-  'am':[ 9, 0, 4 ],
-  'ad':[ 9, 0, 3 ],
-  'a-sus':[ 10, 2, 5 ],
-  'a-susm':[ 10, 1,  5],
-  'a-susd':[ 10, 1, 4 ],
-
-  'b':[ 11, 3, 6 ],
-  'bm':[ 11, 2, 6 ],
-  'bd':[ 11, 2, 5 ]
-}
 
 function changeTone(select){
   const tone = select.value;
-  const tone_chords = tones[tone];
+  const tone_chords = harmonicPlace(tone);
   
   reset();
   
   span_tone.innerHTML = `|| TOM: ${tone} |`;
 
-  fillChordsInKeys(tones[tone]);
+  fillChordsInKeys(tone_chords);
 
   window.onkeypress = ()=>{
 
@@ -99,6 +30,92 @@ function changeTone(select){
 
     activeAcorde(chords[tone_chords[grau]], tone_chords[grau], grau)
   }
+}
+
+
+function harmonicPlace(tone){
+  const type = (tone.indexOf('m') >= 0)?tone_minor:tone_major;
+  const base = notes.indexOf(tone.split('m')[0]);
+  
+  let harmonic_chords_index = [];
+  type.forEach(note=>{
+    const index = (note + base >= 12)?note + base - 12: note + base;
+    harmonic_chords_index.push(index);
+  })
+
+  let harmonic_chords = [];
+  harmonic_chords_index.forEach(tonic_index=>{
+    const harmonic_length = harmonic_chords.length;
+    let chord = notes[tonic_index];
+
+    if (tone.indexOf('m') >= 0) {
+      
+      if (harmonic_length == 0 || harmonic_length == 3 || harmonic_length == 4) {
+        chord += 'm';
+      }
+      else if (harmonic_length == 1) {
+        chord += 'd';
+      }
+    }
+    else{
+
+      if (harmonic_length == 1 || harmonic_length == 2 || harmonic_length == 5) {
+        chord += 'm';
+      }
+      else if (harmonic_length == 6) {
+        chord += 'd';
+      }
+    }
+
+    harmonic_chords.push(chord)
+  })
+
+  fillChords(harmonic_chords);
+
+  return harmonic_chords;
+}
+
+function fillChords(harmonic_scale){
+  chords = {};
+  harmonic_scale.forEach(this_chord=>{
+    const arr_this_chord = this_chord.split('');
+    const length = (arr_this_chord.length-1 == 0)?'':arr_this_chord.length-1;
+    const minor = (arr_this_chord[length] == 'm')?true:false;
+    const diminished = (arr_this_chord[length] == 'd')?true:false;
+
+    let chord_scale = [];
+    if (minor) {
+      let tonic = this_chord.split('');
+      tonic.pop();
+      const base = notes.indexOf(tonic.join(''));
+
+      chord_minor.forEach(index=>{
+        const this_index = (index + base >= 12)? index + base - 12: index + base;
+        chord_scale.push(this_index);
+      })
+    }
+    else if (diminished) {
+      const tonic = this_chord.split('');
+      tonic.pop();
+      const base = notes.indexOf(tonic.join(''));
+
+      chord_diminished.forEach(index=>{
+        const this_index = (index + base >= 12)? index + base - 12: index + base;
+        chord_scale.push(this_index);
+      })
+    }
+    else{
+      const base = notes.indexOf(this_chord);
+
+      chord_major.forEach(index=>{
+        const this_index = (index + base >= 12)? index + base - 12: index + base;
+        chord_scale.push(this_index);
+      })
+    }
+
+    chords[this_chord] = chord_scale;
+    console.log(this_chord, ' -- ',chord_scale, ' -- ', chords)
+  }) 
 }
 
 function reset(){
